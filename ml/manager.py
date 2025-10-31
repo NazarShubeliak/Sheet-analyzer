bbd3b02f48a0675bbdfd6c192a0aeb9504e22c82
Nazar <test@gmail.com>
2025-10-31 13:43:29 +0200

    feat(ml): add ModelManager class and decision three training fucntion

---
+++ ml/manager.py
@@ -0,0 +1,38 @@
+import os
+import joblib
+import pandas as pd
+from .models.desicion_tree import train_reqression_model
+from config.config import logger
+
+class ModelManager:
+    def __init__(self, model_name: str, model_path: str) -> None:
+        self.model_name = model_name
+        self.model_path = model_path
+        self.model = None
+
+        self.__train_fn = {
+            "decision_tree": train_reqression_model
+        }.get(self.model_name)
+
+        if self.__train_fn is None:
+            message = f"Unknow model name {self.model_name}"
+            logger.error(message)
+            raise ValueError(message)
+
+    def model_exists(self) -> bool:
+        return os.path.exists(self.model_path)
+
+    def load(self) -> None:
+        self.model = joblib.load(self.model_path)
+
+    def train(self, df: pd.DataFrame) -> None:
+        self.model = self.__train_fn(df)
+        joblib.dump(self.model, self.model_path)
+
+    def get_or_train(self, df: pd.DataFrame) -> None:
+        if self.model_exists():
+            self.load()
+        else:
+            self.train(df)
+
+    
---
+++ ml/models/desicion_tree.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from config.config import logger
+
+def train_reqression_model(df: pd.DataFrame) -> DecisionTreeRegressor:
+    """
+    Train a DecisionTreeRegressor to predict Total_EUR base on Country and Month
+
+    Args:
+        df (pd.DataFrame): Historical data with 'Country', 'Date', 'Total'
+
+    Returns:
+        DecisionTreeRegressor: Trained model
+    """
+    try:
+        df = df.copy()
+        df["Month"] = pd.to_datetime(df["Date"]).dt.month
+
+        encoder = LabelEncoder()
+        df["Country_encoded"] = encoder.fit_transform(df["Country"])
+
+        x = df["Country_encoded", "Month"]
+        y = df["Total"]
+
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+        model = DecisionTreeRegressor(random_state=42)
+        model.fit(x_train, y_train)
+
+        score = model.score(x_test, y_test)
+        logger.info(f"Model trained with R score: {score:.3f}")
+
+        return model
+    except Exception as e:
+        logger.error(f"Failed to train reqression model: {e}")
+        raise
